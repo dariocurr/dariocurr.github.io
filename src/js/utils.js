b743881ea7ed47540d5175d093a0cd2df344bbfe
Dario Curreri <dariocurr@gmail.com>
2022-06-25 14:52:39 +0000

    update utils

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,10 +1,7 @@
 function clean(string) {
-    return $('<textarea/>').html(string).text().replace(/\s+/g, " ").replaceAll("<br> ", "\n").trim();
+    return string.replace(/\s+/g, " ").replaceAll("<br>", "\n").trim();
 }
 
 function createLink(link) {
-    if (link.startsWith("assets/")) {
-        return "https://dariocurr.github.io/" + link;
-    }
-    return link;
+    return link.startsWith("assets/") ? "https://dariocurr.github.io/" + link : link
 }
